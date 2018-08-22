6d8401980b39a95c2396d1ec9cb5c481950cfe56
jiro4989 <jiroron666@gmail.com>
2018-08-22 22:05:41 +0900

    エラーが出るパターンテスト追加

--- main_test.go
+++ main_test.go
@@ -25,6 +25,28 @@ func TestPlt(t *testing.T) {
 		err := plt(v.args, v.opts)
 		assert.Equal(t, v.err, err)
 	}
+
+	testdatas = []TestPltData{
+		TestPltData{
+			// 引数未指定の場合
+			args: []string{},
+			opts: options.Options{},
+		},
+		TestPltData{
+			// 存在しない入力ファイル
+			args: []string{"hogefugatmp", "testdata/out/hogefugatmp.png"},
+			opts: options.Options{},
+		},
+		TestPltData{
+			// 出力先ディレクトリが存在しない
+			args: []string{"testdata/in/value_only.tsv", "testdata/hogefugatmp/value_only.png"},
+			opts: options.Options{},
+		},
+	}
+	for _, v := range testdatas {
+		err := plt(v.args, v.opts)
+		assert.Error(t, err)
+	}
 }
 
 func TestWriteGraph(t *testing.T) {
