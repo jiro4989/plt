111e0eec4092ae1b578a70f1c40bd06b47fbe10c
jiro4989 <jiroron666@gmail.com>
2018-08-22 23:53:54 +0900

    行列計算のtテストコード追加

--- graph/graph.go
+++ graph/graph.go
@@ -3,6 +3,7 @@ package graph
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	options "github.com/jiro4989/plt/internal/options"
 	"gonum.org/v1/plot/plotter"
@@ -13,28 +14,79 @@ type Graph struct {
 	Data  plotter.XYs
 }
 
-func GetGraphs(lines []string, opts options.Options) ([]Graph, error) {
-	var title string
-	p := make(plotter.XYs, len(lines)-1)
-	for i, v := range lines {
-		if i == 0 {
-			title = v
-		} else {
-			n, err := strconv.ParseFloat(v, 64)
-			if err != nil {
-				fmt.Println("error")
-				continue
-			}
-			p[i-1].X = float64(i - 1)
-			p[i-1].Y = n
-		}
+// reshape は行列を入れ替えます。
+// ex:
+//   in:
+//     1,2,3
+//     4,5,6
+//   out:
+//     1,4
+//     2,5
+//     3,6
+func reshape(s [][]string) [][]string {
+	if len(s) <= 0 {
+		return [][]string{}
 	}
 
-	g := []Graph{
-		Graph{
+	rl := len(s)
+	cl := len(s[0])
+	matrix := make([][]string, cl)
+	for i := range matrix {
+		matrix[i] = make([]string, rl)
+	}
+
+	for i, r := range s {
+		for j, c := range r {
+			matrix[j][i] = c
+		}
+	}
+	return matrix
+}
+
+// splitLines は配列文字列を指定の文字列で更に分割した二次元配列を返す。
+// セパレータがから文字列の場合はそのまま文字列を二次元配列で返す。
+func splitLines(lines []string, sep string) [][]string {
+	if sep == "" {
+		return [][]string{lines}
+	}
+
+	splited := make([][]string, len(lines))
+	for i, line := range lines {
+		ss := strings.Split(line, sep)
+		ns := make([]string, len(ss))
+		for i, v := range ss {
+			ns[i] = strings.TrimSpace(v)
+		}
+		splited[i] = ns
+	}
+	return reshape(splited)
+}
+
+func GetGraphs(lines []string, opts options.Options) ([]Graph, error) {
+	matrix := splitLines(lines, opts.Separator)
+	gs := make([]Graph, len(matrix))
+	for x, ls := range matrix {
+		var title string
+		p := make(plotter.XYs, len(ls)-1)
+		for i, v := range ls {
+			if i == 0 {
+				title = v
+			} else {
+				n, err := strconv.ParseFloat(v, 64)
+				if err != nil {
+					fmt.Println("error")
+					continue
+				}
+				p[i-1].X = float64(i - 1)
+				p[i-1].Y = n
+			}
+		}
+		g := Graph{
 			Title: title,
 			Data:  p,
-		},
+		}
+		gs[x] = g
 	}
-	return g, nil
+
+	return gs, nil
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -8,6 +8,108 @@ import (
 	"gonum.org/v1/plot/plotter"
 )
 
+type TestReshapeData struct {
+	in  [][]string
+	out [][]string
+}
+
+func TestReshape(t *testing.T) {
+	testdatas := []TestReshapeData{
+		TestReshapeData{
+			in: [][]string{
+				{"1", "2", "3"},
+				{"4", "5", "6"},
+				{"7", "8", "9"},
+			},
+			out: [][]string{
+				{"1", "4", "7"},
+				{"2", "5", "8"},
+				{"3", "6", "9"},
+			},
+		},
+		TestReshapeData{
+			in: [][]string{
+				{"1", "2"},
+				{"3", "4"},
+			},
+			out: [][]string{
+				{"1", "3"},
+				{"2", "4"},
+			},
+		},
+		TestReshapeData{
+			in: [][]string{
+				{"1", "2"},
+			},
+			out: [][]string{
+				{"1"},
+				{"2"},
+			},
+		},
+		TestReshapeData{
+			in:  [][]string{{"1"}},
+			out: [][]string{{"1"}},
+		},
+		TestReshapeData{
+			in:  [][]string{},
+			out: [][]string{},
+		},
+	}
+	for _, v := range testdatas {
+		assert.Equal(t, v.out, reshape(v.in))
+	}
+}
+
+type TestSplitLinesData struct {
+	lines []string
+	sep   string
+	out   [][]string
+}
+
+func TestSplitLines(t *testing.T) {
+	testdatas := []TestSplitLinesData{
+		TestSplitLinesData{
+			lines: []string{"value", "1"},
+			sep:   "\t",
+			out:   [][]string{[]string{"value", "1"}},
+		},
+		TestSplitLinesData{
+			lines: []string{"value1\tvalue2", "1\t2"},
+			sep:   "\t",
+			out: [][]string{
+				[]string{"value1", "1"},
+				[]string{"value2", "2"},
+			},
+		},
+		TestSplitLinesData{
+			lines: []string{"value1,value2", "1,2"},
+			sep:   ",",
+			out: [][]string{
+				[]string{"value1", "1"},
+				[]string{"value2", "2"},
+			},
+		},
+		TestSplitLinesData{
+			lines: []string{"\t  value1   ,   value2  \t ", "1 \t  ,\t    2"},
+			sep:   ",",
+			out: [][]string{
+				[]string{"value1", "1"},
+				[]string{"value2", "2"},
+			},
+		},
+		TestSplitLinesData{
+			lines: []string{"value1,value2", "1,2"},
+			sep:   "",
+			out: [][]string{
+				[]string{"value1,value2", "1,2"},
+			},
+		},
+	}
+	for _, v := range testdatas {
+		assert.Equal(t, v.out, splitLines(v.lines, v.sep))
+	}
+}
+
 type TestGetGraphsData struct {
 	in  []string
 	out []Graph
@@ -43,6 +145,27 @@ func TestGetGraphs(t *testing.T) {
 				Graph{
 					Title: "value",
 					Data:  f(1, 3, 5, 7, 9),
+				},
+			},
+			err: nil,
+		},
+		TestGetGraphsData{
+			in: []string{
+				"value1\tvalue2",
+				"1\t3",
+				"3\t5",
+				"5\t7",
+				"7\t9",
+				"9\t11",
+			},
+			out: []Graph{
+				Graph{
+					Title: "value1",
+					Data:  f(1, 3, 5, 7, 9),
+				},
+				Graph{
+					Title: "value2",
+					Data:  f(3, 5, 7, 9, 11),
 				},
 			},
 			err: nil,
