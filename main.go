5578da55a53bdedc4dca5baaed7e2cb21abe0ed4
jiro4989 <jiroron666@gmail.com>
2018-08-23 01:06:36 +0900

    サンプルコード削除

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
-	"math/rand"
 	"os"
 	"strings"
 
@@ -12,7 +11,6 @@ import (
 	"github.com/jiro4989/plt/graph"
 	options "github.com/jiro4989/plt/internal/options"
 	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 )
@@ -119,18 +117,4 @@ func writeGraph(lines []string, opts options.Options, ofn string) error {
 		return err
 	}
 	return nil
-}
-
-// randomPoints returns some random x, y points.
-func randomPoints(n int) plotter.XYs {
-	pts := make(plotter.XYs, n)
-	for i := range pts {
-		if i == 0 {
-			pts[i].X = rand.Float64()
-		} else {
-			pts[i].X = pts[i-1].X + rand.Float64()
-		}
-		pts[i].Y = pts[i].X + 10*rand.Float64()
-	}
-	return pts
 }
